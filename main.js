a8be8942f4c20c2cf9e76be228afa3d6203850d5
Shohijahon Musinkulov <shohijahonmusinkulov@gmail.com>
2024-10-05 19:22:02 +0500

    Add git comments

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 console.log("Version 1");
+
+// git branch branch_name (Branch yaratish)
+// git branch -M branch_name (Local main uzgartirish)
+// git checkout branch_name (Mavjud bo'lgan branchga kirib beradi)
+// git checkout -b branch_name (BRANCH CREATE -> git checkout BRANCH_NAME)
